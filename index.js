354bad4efa6d5b8d7734f24c7021e6356fa2af22
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-23 02:22:30 +0300

    more ux design

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 
-document.addEventListener("DOMContentLoaded", displayBooks());
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", displayBooks()); //event listener forloaded document
+
+
 
 function displayBooks(){
     let displayBlock = document.getElementById("display-books");
-    console.log("displays books from the api");
     fetch("https://www.anapioficeandfire.com/api/books")
     .then(response => response.json())
     .then(function(data){
         for(book of data){
-            console.log(book.name);
             let bookContainer = document.createElement("h2");
             let buttons = document.createElement("input")
-            buttons.setAttribute("type", "button")
-            buttons.setAttribute("placeholder", "Know More");
+            buttons.setAttribute("type", "submit")
             buttons.className = "info-button";
+            buttons.addEventListener("submit", displayInfo());
             bookContainer.textContent = book.name;
             bookContainer.appendChild(buttons);
             displayBlock.appendChild(bookContainer);
         }
     })
+}
+
+
+function displayInfo(){
+    console.log("working");
 }
